f8bbb1bda697f258846ef21fd0d8d2274e19e2c5
Rivaldo Stuart Wattimena <rivaldo.wattimena@shopee.com>
2022-11-17 17:38:41 +0700

    refactor: add json tag

--- domain/coupon.go
+++ domain/coupon.go
@@ -5,18 +5,18 @@ import (
 )
 
 type Coupon struct {
-	Id        string    `gorm:"primaryKey;column:id"`
-	Code      string    `gorm:"column:code"`
-	Discount  int       `gorm:"column:discount"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-	DeletedAt time.Time `gorm:"column:deleted_at"`
+	Id        string    `gorm:"primaryKey;column:id" json:"id"`
+	Code      string    `gorm:"column:code" json:"code"`
+	Discount  int       `gorm:"column:discount" json:"discount"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	DeletedAt time.Time `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 type UserCoupon struct {
-	UserId    string    `gorm:"column:user_id"`
-	CouponId  string    `gorm:"column:coupon_id"`
-	ExpiredAt time.Time `gorm:"column:expired_at"`
-	Coupon    Coupon    `gorm:"foreignKey:CouponId;references:Id"`
+	UserId    string    `gorm:"column:user_id" json:"user_id"`
+	CouponId  string    `gorm:"column:coupon_id" json:"coupon_id"`
+	ExpiredAt time.Time `gorm:"column:expired_at" json:"expired_at"`
+	Coupon    Coupon    `gorm:"foreignKey:CouponId;references:Id" json:"coupon"`
 }
 
 type UserCoupons []UserCoupon
