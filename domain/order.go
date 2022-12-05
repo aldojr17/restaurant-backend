86d90771beac5aa4ffcdd376f0e3f9654d190a9d
Rivaldo Stuart Wattimena <rivaldo.wattimena@shopee.com>
2022-12-05 16:29:15 +0700

    refactor: delete unused attributes

--- domain/order.go
+++ domain/order.go
@@ -10,7 +10,6 @@ type Order struct {
 	Id           int           `gorm:"primaryKey;column:id" json:"id"`
 	UserId       string        `gorm:"column:user_id" json:"user_id"`
 	CouponId     *string       `gorm:"column:coupon_id" json:"coupon_id"`
-	Notes        *string       `gorm:"column:notes" json:"notes"`
 	PaymentId    int           `gorm:"column:payment_id" json:"payment_id"`
 	Status       string        `gorm:"column:status" json:"status"`
 	OrderDate    time.Time     `gorm:"column:order_date" json:"order_date"`
@@ -27,7 +26,6 @@ type OrderPayload struct {
 	Id         int `json:"id"`
 	UserId     string
 	CouponId   *string              `json:"coupon_id"`
-	Notes      *string              `json:"notes"`
 	PaymentId  int                  `json:"payment_id"`
 	Status     string               `json:"status"`
 	OrderDate  time.Time            `json:"order_date"`
